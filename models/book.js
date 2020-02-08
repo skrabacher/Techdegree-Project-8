1763019dc41561702ce03629f3433c577014291d
skrabacher <37253871+skrabacher@users.noreply.github.com>
2020-02-07 18:44:26 -0800

    fixed validation issue
    *changed update-book to book-detail
    *notNull to notEmpty in book model file
    *added page not found pug view

--- models/book.js
+++ models/book.js
@@ -5,7 +5,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING, 
       allowNull: false, // disallow null requires a value for data to be submitted to db
       validate: {
-        notNull: {
+        notEmpty: {
             msg: 'Please provide a value for "title"',
         }
       },
@@ -14,7 +14,7 @@ module.exports = (sequelize, DataTypes) => {
       type:  DataTypes.STRING,
       allowNull: false, // disallow null requires a value for data to be submitted to db
       validate: {
-        notNull: {
+        notEmpty: {
             msg: 'Please provide a value for "author"',
         }
       },
--- routes/books.js
+++ routes/books.js
@@ -1,7 +1,7 @@
 //
 const express = require('express'); //requires express functionality(for routing)
 const router = express.Router();
-const Book = require('../models').Book;//gives access to the book model file(book.js)
+const Book = require('../models').Book; //gives access to the book model file(book.js)
 
 /* Handler function to wrap each route. (eliminates need to write try/catch over and over in each route)*/
 function asyncHandler(cb){
@@ -39,12 +39,14 @@ router.post('/new', asyncHandler(async (req, res) => {
   try {
     console.log("post books/new: ", req.body) //checking to make sure it returns: { title: '', author: '', body: '' } ---- (and obj w/ props that map to the model attributes)
     const book = await Book.create(req.body); //req.body is the body of your request NOT equivalent to the body property of an book object//create requires an obj with props that map to the model attributes (book.js - title, author, genre, year)
-    res.redirect("/" + book.id); //id concatenated on the end creates a unique url path for each query based on the id column
+    //res.render("/form-error", { error });
+    res.redirect("/books"); 
   } catch (error) {
+    console.log("error: ", error.name, error.status);
     if(error.name === "SequelizeValidationError") { //if validation error, render the form-error pug view and pass the error object as local var to pug view
       book = await Book.build(req.body); 
       console.log("validation error req.body: ", req.body);
-      res.render("/form-error", { error })
+      res.render("form-error", { error });
     } else {
       throw error; 
     }  
@@ -60,7 +62,7 @@ router.post('/new', asyncHandler(async (req, res) => {
 router.get("/:id", asyncHandler(async (req, res) => {
   const book = await Book.findByPk(req.params.id); //Book=book module, then find by using the number entered in the route as a parameter defined as id
   console.log("CONSOLE LOG: ", book); //just to see what the returned data looks like
-  res.render("book-detail", { book: book, title: book.title, id: book.id }); //book: book - the first book is the container for the local variable that is passed to pug
+  res.render("update-book", { book: book, title: book.title, id: book.id }); //book: book - the first book is the container for the local variable that is passed to pug
   //render method defaults to views path as defined in the app.js file with app.set in the view engine setup [app.set('views', path.join(__dirname, 'views'));]
 }));
 
